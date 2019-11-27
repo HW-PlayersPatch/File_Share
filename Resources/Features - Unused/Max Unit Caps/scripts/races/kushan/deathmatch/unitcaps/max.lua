ca85f3be654cd2f7c8ec84a7b7bc931fe81d7c9a
Cloaked <cloaked2222@outlook.com>
2019-11-27 17:47:25 -0500

    .

---
+++ Resources/Features - Unused/Max Unit Caps/scripts/races/kushan/deathmatch/unitcaps/max.lua
@@ -0,0 +1,5 @@
+--Kushan
+supplyLimit("Frigate", 250);
+supplyLimit("Carrier", 250);
+
+supplyDesc("Battlecruiser", "HeavyCruiser");
---
+++ Resources/Features - Unused/Max Unit Caps/scripts/races/vaygr/deathmatch/unitcaps/max.lua
@@ -0,0 +1,11 @@
+-- Vaygr
+supplyLimit("Fighter", 250);
+supplyLimit("Scout", 250);
+supplyLimit("Interceptor", 250);
+supplyLimit("Bomber", 250);
+supplyLimit("LanceFighter", 250);
+supplyLimit("Corvette", 250);
+supplyLimit("Capital", 250);
+supplyLimit("Carrier", 250);
+
+
